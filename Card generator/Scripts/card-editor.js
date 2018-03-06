d50c5f087b9cf649ff3e2ef468aef6e441da2177
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-03-05 22:30:18 -0800

    Checking in - changes in progress to create an editor. Breaks current download functionality.

---
+++ Card generator/Scripts/card-editor.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $(".card-label").click(function(){
+        var $this = $(this);
+        var $labelInput = $("#labelInput");
+        console.log($this.text());
+        $labelInput.val($this.text());
+
+    });
+});

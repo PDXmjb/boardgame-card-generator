40d152304b3237bd2b8d23cb2f14d1756736eef0
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-03-10 17:12:43 -0800

    Adding style editor functionality. Needed to change from .prop to .attr to correctly pull the <style> tag.

--- Card generator/Scripts/card-editor.js
+++ Card generator/Scripts/card-editor.js
@@ -4,15 +4,16 @@
         $this = $(this);
         var $labelInput = $("#labelInput");
         $labelInput.val($this.text());
-        //var $cssInput = $("#cssInput");
-        //$cssInput.val($this.prop("style"));
-        console.log($this.prop("style"));
+        var $cssInput = $("#cssInput");
+        $cssInput.val($this.attr("style"));
+        console.log($this.attr("style"));
     });
 
     $("#preview-button").click(function () {
         if ($this)
         {
             $this.text($("#labelInput").val());
+            $this.attr("style", $("#cssInput").val());
         }
     });
 });

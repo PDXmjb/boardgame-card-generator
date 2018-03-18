13d6d31d4992f6fe3eabf648f0054cb0493f869e
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-03-17 20:06:23 -0700

    Creating SQL for the AWS table, including basic testing data

--- Card generator/App_Data/dbo.Game.sql
+++ Card generator/App_Data/dbo.Game.sql
@@ -1,6 +1,61 @@
-﻿CREATE TABLE [dbo].[Game]
+﻿CREATE TABLE [dbo].[Owner]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(50) NOT NULL, 
-    [OwnerId] INT NOT NULL
+    [OwnerName] NVARCHAR(100) NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC)
 )
+GO
+
+CREATE TABLE [dbo].[Game]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [GameName] NVARCHAR(100) NOT NULL,
+	[OwnerId] INT NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Game_Owner] FOREIGN KEY ([OwnerId]) REFERENCES [Owner]([Id])
+)
+GO
+
+CREATE TABLE [dbo].[Deck]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [DeckName] NVARCHAR(100) NOT NULL,
+	[GameId] INT NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Deck_Game] FOREIGN KEY ([GameId]) REFERENCES [Game]([Id])
+)
+GO
+
+CREATE TABLE [dbo].[Card]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[DeckId] INT NOT NULL,
+	[BackgroundImage] NVARCHAR(300),
+	[BackgroundColor] NVARCHAR(300),
+	PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Card_Deck] FOREIGN KEY ([DeckId]) REFERENCES [Deck]([Id])
+)
+GO
+
+CREATE TABLE [dbo].[LabelPosition]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[Name] VARCHAR(20) NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[CardLabel]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[CardId] INT NOT NULL,
+	[LabelPositionId] INT NOT NULL,
+	[BackgroundImage] NVARCHAR(300),
+	[BackgroundColor] NVARCHAR(300),
+	[AdditionalCSS] NVARCHAR(500),
+	[Label] NVARCHAR(200),
+	PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_CardLabel_Card] FOREIGN KEY ([CardId]) REFERENCES [Card]([Id]),
+	CONSTRAINT [FK_CardLabel_LabelPosition] FOREIGN KEY ([LabelPositionId]) REFERENCES [LabelPosition]([Id])
+)
+GO
+
--- Database setup.sql
+++ Database setup.sql
@@ -1,4 +1,11 @@
-﻿CREATE TABLE [dbo].[Owner]
+﻿SELECT * FROM [Owner]
+
+INSERT INTO [Owner] (OwnerName) VALUES ('Mike') GO
+INSERT INTO [Game] (GameName, OwnerId) VALUES ('CrusaderCards', 1) GO
+INSERT INTO [Deck] (DeckName, GameId) VALUES ('AllCards', 1) GO
+
+
+CREATE TABLE [dbo].[Owner]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL, 
     [OwnerName] NVARCHAR(100) NOT NULL,
@@ -37,37 +44,26 @@ CREATE TABLE [dbo].[Card]
 )
 GO
 
+CREATE TABLE [dbo].[LabelPosition]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[Name] VARCHAR(20) NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+GO
+
 CREATE TABLE [dbo].[CardLabel]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL, 
 	[CardId] INT NOT NULL,
-	[Row] INT NOT NULL,
-	[Column] INT NOT NULL,
+	[LabelPositionId] INT NOT NULL,
 	[BackgroundImage] NVARCHAR(300),
 	[BackgroundColor] NVARCHAR(300),
 	[AdditionalCSS] NVARCHAR(500),
 	[Label] NVARCHAR(200),
 	PRIMARY KEY CLUSTERED ([Id] ASC), 
     CONSTRAINT [FK_CardLabel_Card] FOREIGN KEY ([CardId]) REFERENCES [Card]([Id]),
+	CONSTRAINT [FK_CardLabel_LabelPosition] FOREIGN KEY ([LabelPositionId]) REFERENCES [LabelPosition]([Id])
 )
 GO
 
-
-INSERT INTO [Owner] (OwnerName) 
-VALUES ('Mike Brooks');
-
-INSERT INTO Game (GameName, OwnerId) 
-VALUES ('CrusaderCards', 1);
-
-INSERT INTO Deck (DeckName, GameId) 
-VALUES ('Deck1', 1);
-
-INSERT INTO [Card] (DeckId, BackgroundImage, BackgroundColor) 
-VALUES (1, 'https://i.imgur.com/nYhlAzc.jpg', '#555');
-
-INSERT INTO CardLabel (CardId, [Row], [Column], [BackgroundImage], [BackgroundColor], [AdditionalCSS], Label) 
-VALUES (1, 1, 1, null, 'purple', 'border: 1px solid black; border-radius:40px;', '1VP');
-INSERT INTO CardLabel (CardId, [Row], [Column], [BackgroundImage], [BackgroundColor], [AdditionalCSS], Label) 
-VALUES (1, 1, 2, null, null, 'font-size: 50px', 'Knight');
-INSERT INTO CardLabel (CardId, [Row], [Column], [BackgroundImage], [BackgroundColor], [AdditionalCSS], Label) 
-VALUES (1, 2, 2, 'https://i.imgur.com/NYF2Ki7.jpg', null, 'border: 1px solid black; border-radius:40px;', null);
